651f006b215d6a671793e336e0c5453fc1826825
andreitch <andreitch.top@gmail.com>
2018-08-30 19:42:27 +0300

    Managing the test database on staging

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,8 +3,9 @@ import time
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .server_tools import reset_database
 
-MAX_WAIT = 10 
+MAX_WAIT = 10
 
 
 def wait(fn):
@@ -20,35 +21,36 @@ def wait(fn):
     return modified_fn
 
 class FunctionalTest(StaticLiveServerTestCase):
-	
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
-                
+
     @wait
     def wait_for(self, fn):
         return fn()
-        
+
     @wait
     def wait_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-                
+
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
-    
+
     @wait
     def wait_to_be_logged_in(self, email):
         self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
-       
+
     @wait
     def wait_to_be_logged_out(self, email):
         self.browser.find_element_by_name('email')

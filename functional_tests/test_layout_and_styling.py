3df48ca3df50e72ad2c466464f431ee4b44c8f81
andreitch <andreitch.top@gmail.com>
2018-08-31 15:08:18 +0300

    FT for 'My lists' + generic add list item re-factoring

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.keys import Keys
 
 
 class LayoutAndStylingTest(FunctionalTest):
-	    
+
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        
+
         # She notices the input box is nicely centered
         inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
@@ -16,12 +16,10 @@ class LayoutAndStylingTest(FunctionalTest):
             512,
             delta=10
         )
-        
+
         # She starts a new list and sees the input is nicely
         # centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
+        self.add_list_item('testing')
         inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
